35d7d57b3cae0fa5d43b6693fcda35502d32ed86
Thaphat Meechaitana <128665086+ThaphatTT@users.noreply.github.com>
2024-04-08 23:32:08 +0700

    create api about update ticket's data information & status

--- ticket-management-app/server/app.js
+++ ticket-management-app/server/app.js
@@ -35,6 +35,21 @@ app.get('/', (req,res)=>{
   })
 })
 
+app.get('/ticket/:id', (req,res)=>{
+  pool.getConnection((err, connection) =>{
+    if(err) throw err
+    console.log(`connected as id ${connection.threadId}`);
+    connection.query('SELECT * from ticket WHERE id = ?',[req.params.id] ,(err, rows) =>{
+      connection.release()
+      if(!err){
+        res.send(rows)
+      }else{
+        console.log(err);
+      }
+    })
+  })
+})
+
 app.post('/create', (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
@@ -63,6 +78,32 @@ app.post('/create', (req, res) => {
       }
     });
   });
+});
+
+app.put('/updateTicket/:id', (req, res) => {
+  pool.getConnection((err, connection) => {
+    const data = {
+      Title: req.body.Title,
+      Description: req.body.Description,
+      ContactInformation: req.body.ContactInformation,
+      newTimestamp: new Date(),
+      newStatus: req.body.Status
+    };
+    
+    let sqlTicket = `UPDATE ticket SET Title = ?, Description = ?, ContactInformation = ?, Status = ?, LastestTicketTimeStamp = ? WHERE id = ?`;
+    let sqlLastestTicket = `INSERT INTO LastestTicket (TicketId, LastestTicketTimeStamp, Status) VALUES (?, ?, ?)`;
+
+    connection.query(sqlTicket, [data.Title, data.Description, data.ContactInformation, data.newStatus, data.newTimestamp, req.params.id], (err, result) => {
+        if (err) throw err;
+        connection.query(sqlLastestTicket, [req.params.id, data.newTimestamp, data.newStatus], (err, result) => {
+            if (err) throw err;
+            res.send({
+              message : 'Update & Insert Data Sucess!!',
+              data : data
+            });
+        });
+    });
+  })
 });
 
 
